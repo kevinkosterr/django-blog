738ae65ae320cbd774ae3484fe73c8c4da91057e
kevinkosterr <koster.k2001@gmail.com>
2021-06-24 19:10:06 +0200

    feat(graphql): To-do items using GraphQL

--- core/todolist/graphql/query.py
+++ core/todolist/graphql/query.py
@@ -10,15 +10,19 @@ class ToDoModel(DjangoObjectType):
 
 
 class ToDoQuery(graphene.ObjectType):
-    ToDoItem = graphene.Field(
-        ToDoModel,
-        id=graphene.Int(),
-        author=graphene.String(),
-        completed=graphene.Boolean(),
-    )
+    ToDoItem = graphene.Field(ToDoModel, id=graphene.Int())
+    ToDoItems = graphene.List(ToDoModel)
 
-    def resolve_todoitem(self, info, **kwargs):
+    def resolve_ToDoItem(self, info, **kwargs):
         _id = kwargs.get("id")
 
         if _id is not None:
             return ToDoItem.objects.get(id=_id)
+
+        return None
+
+    def resolve_ToDoItems(self, info, **kwargs):
+        limit = kwargs.get("limit")
+        if not limit:
+            return ToDoItem.objects.all()
+        return ToDoItem.objects.all()[:limit]
